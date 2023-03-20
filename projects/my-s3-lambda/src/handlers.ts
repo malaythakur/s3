8608e5f0440480e8ea32736c9b04a0055884080e
devpeak <malaythakur13@gmail.com>
2023-03-20 20:17:18 +0530

    getUser added

--- projects/my-s3-lambda/src/handlers.ts
+++ projects/my-s3-lambda/src/handlers.ts
@@ -1,15 +1,53 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { S3 } from "aws-sdk";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
-  };
+
+const s3 = new S3();
+
+export const getUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const uuid = event.pathParameters!["uuid"];
+
+
+  if (!uuid){
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error:"missing UUID"}),
+    };
+
+  }
+  try{
+  await s3.headObject({
+    Bucket: "malay-s3-bucket",
+    Key: `${uuid}.json`,
+  }).promise();
+
+  const output = s3.getObject({
+      Bucket: "bucketName",
+      Key: `${uuid}.json`,
+    }).promise();
+
+    return{
+      statusCode:200,
+      body: (await output).Body?.toString() || "",
+    };
+  }catch (e) {
+    if(e.code === "NotFound" || e.code === "NoSuchKey"){
+      
+      return{
+        statusCode: 404,
+        body: JSON.stringify({error:"user not found"}),
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: e.message
+      }),
+    }
+
+  }
+  
+
+
 };
